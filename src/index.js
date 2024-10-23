967f7550f20fba8d6d389b3be7dad9faa3394c8d
Tautvydas Dulskis <tautvydas.dulskis@venipak.com>
2024-10-23 19:06:54 +0300

    Static dir ands css + includes of partials

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ const app = express();
 
 app.listen(3000);
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname));
 
-app.get('/', (req, res) => { res.render('index', {vardas: "Tautvydai", title: "Pradzia"} )});
+const masyvas = [
+    {title: "Sutvarkyti kambarius", id: 1, author: "Tautvydas", status: "done"},
+    {title: "Kitas darbas", author: "Tautvydas", id: 2,  status: "in progress"},
+    {author: "Tautvydas", title: "Trecias darbas", id: 3, status: "todo"},
+];
+
+app.get('/', (req, res) => { res.render('index',{duomenys: masyvas, title: "Home"} )});
 app.get('/about', (req, res) => {  res.render('about', {title: "About us"})});
 app.get('/apie-mane', (req, res) => {  res.redirect('about')});
+app.get('/slapta-info', (req, res) => {  res.redirect('about')});
 app.use((req, res) => { res.status(404).render('404', {title: "404"})});
