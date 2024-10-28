8a54a4fb8e18a2655d8bda2ecfc441430905fd3e
Tautvydas Dulskis <tautvydas.dulskis@venipak.com>
2024-10-28 19:30:12 +0200

    Migrating to ToDo app 1.0.0 and preparing to store data in files

--- src/index.js
+++ src/index.js
@@ -1,19 +1,41 @@
+'use strict';
+
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
 app.listen(3000);
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname));
+app.use(express.json());
 
-const masyvas = [
-    {title: "Sutvarkyti kambarius", id: 1, author: "Tautvydas", status: "done"},
-    {title: "Kitas darbas", author: "Tautvydas", id: 2,  status: "in progress"},
-    {author: "Tautvydas", title: "Trecias darbas", id: 3, status: "todo"},
-];
+app.get('/todo', (req, res) => {
+    //... skaitom failo turini ir priskiriam kintamajam masyvas
+    res.json(masyvas);
+});
 
-app.get('/', (req, res) => { res.render('index',{duomenys: masyvas, title: "Home"} )});
-app.get('/about', (req, res) => {  res.render('about', {title: "About us"})});
-app.get('/apie-mane', (req, res) => {  res.redirect('about')});
-app.get('/slapta-info', (req, res) => {  res.redirect('about')});
-app.use((req, res) => { res.status(404).render('404', {title: "404"})});
+app.get('/todo/:id', (req, res) => {
+    const id = req.params.id;
+    //... skaitom failo turini ir priskiriam kintamajam masyvas
+    const elementas = masyvas.find(elementas => elementas.id == id);
+    if (!elementas) {
+        res.json({error: "Elementas nerastas"});
+        return;
+    }
+    res.json(elementas);
+});
+
+app.post('/todo', (req, res) => {
+    //... skaitom failo turini ir priskiriam kintamajam masyvas
+    const title = req.body.title;
+    const author = req.body.author;
+    const status = req.body.status;
+    const id = masyvas.length + 1;
+    // masyvas.push({title, author, status, id}); // reikia refaktorinti saugojima i faila
+    res.json(masyvas);
+});
+
+app.use((req, res) => {
+    res.status(404).json({error: "Puslapis nerastas"});
+});
