d74f4fb895945758266638cf831b724f97a404fa
Tautvydas Dulskis <tautvydas.dulskis@venipak.com>
2024-11-20 18:45:52 +0200

    NodeJs Authentification logic

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,7 +3,7 @@ const User = require('../models/user');
 const bcrypt = require('bcrypt');
 
 const authorize = async (req, res, next) => {
-    const { email, password } = req.body;
+    const { email, password } = req.headers;
     const user = await User.findOne({ email });
     if (!user || !bcrypt.compareSync(password, user.password)) {
         return res.status(401).json({ message: 'Neteisingi prisijungimo duomenys' });
